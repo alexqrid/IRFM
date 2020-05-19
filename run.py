ef8aea1d6c17cc7952fb613360b0eafa95e85b6d
Alexander Gridin <alexqrid@gmail.com>
2020-05-19 16:19:58 +0300

    quick fix

--- run.py
+++ run.py
@@ -17,26 +17,25 @@ if not os.path.exists(app.config['STATIC_DIR'] + '/reports'):
     """При первом запуке загружаем и индексируем все пдф, поэтому запуск долгий
     проверяем также наличие индекса в эластике"""
     os.mkdir(app.config['STATIC_DIR'] + '/reports')
-else:
-    if not es.indices.exists(index='reports'):
-        if not create_index(es):
-            os._exit(1)
-        for year, link in links.items():
-            if int(year) < 2000:
-                break
-            path = f'{app.config["STATIC_DIR"]}/reports/report{year}.pdf'
-            if os.path.exists(path):
-                with open(path, "rb") as fd:
-                    document = fd.read()
-            else:
-                with open(path, 'wb+') as fd:
-                    document = download(link)
-                    fd.write(download(link))
-            es.index(index='reports', doc_type="pdf", id=year, pipeline='attachment',
-                         body={
-                                 "data": (b64(document)).decode('ascii'),
-                             }
-                         )
+if not es.indices.exists(index='reports'):
+    if not create_index(es):
+        os._exit(1)
+    for year, link in links.items():
+        if int(year) < 2000:
+            break
+        path = f'{app.config["STATIC_DIR"]}/reports/report{year}.pdf'
+        if os.path.exists(path):
+            with open(path, "rb") as fd:
+                document = fd.read()
+        else:
+            with open(path, 'wb+') as fd:
+                document = download(link)
+                fd.write(download(link))
+        es.index(index='reports', doc_type="pdf", id=year, pipeline='attachment',
+                     body={
+                             "data": (b64(document)).decode('ascii'),
+                         }
+                     )
 
 
 app.run(host="127.0.0.1", port=8080, debug=True)
